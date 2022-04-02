d744e93203476dd59c0cc4722300173d5f0a95c1
Abhishek Biswas <abhishek.biswas.mail@gmail.com>
2022-04-02 16:00:33 +0530

    Renamed methods.

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-function compare(item1, item2) {
+function any(item1, item2) {
   if (typeof item1 !== typeof item2) return false;
   switch (typeof item1) {
     case "function":
-      if (!compareFn(item1, item2)) return false;
+      if (!fnc(item1, item2)) return false;
       return true;
     case "object":
       if (item1) {
         if (item1.length === undefined) {
-          if (!compareObj(item1, item2)) return false;
+          if (!obj(item1, item2)) return false;
           return true;
         } else {
-          if (!compareArr(item1, item2)) return false;
+          if (!arr(item1, item2)) return false;
           return true;
         }
       } else {
@@ -25,42 +25,42 @@ function compare(item1, item2) {
         if (isNaN(item2)) return true;
         return false;
       }
-      if (!compareRest(item1, item2)) return false;
+      if (!rest(item1, item2)) return false;
       return true;
   }
 }
 
-function compareFn(function1, function2) {
+function fnc(function1, function2) {
   if (function1.toString() !== function2.toString()) return false;
   return true;
 }
 
-function compareObj(object1, object2) {
+function obj(object1, object2) {
   if (Object.keys(object1).length === Object.keys(object2).length) {
     for (key in object1) {
-      if (!compare(object1[key], object2[key])) return false;
+      if (!any(object1[key], object2[key])) return false;
     }
     return true;
   }
 }
 
-function compareArr(array1, array2) {
+function arr(array1, array2) {
   if (array1.length === array2.length) {
     array1.sort();
     array2.sort();
     for (i = 0; i < array1.length; i++) {
-      if (!compare(array1[i], array2[i])) return false;
+      if (!any(array1[i], array2[i])) return false;
     }
     return true;
   }
 }
 
-function compareRest(item1, item2) {
+function rest(item1, item2) {
   if (item1 !== item2) return false;
   return true;
 }
 
-module.exports.compare = compare;
-module.exports.compareFn = compareFn;
-module.exports.compareObj = compareObj;
-module.exports.compareArr = compareArr;
+module.exports.any = any;
+module.exports.fnc = fnc;
+module.exports.obj = obj;
+module.exports.arr = arr;
